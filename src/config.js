c2769535f53d56aabb875acc6d424ac34c39b997
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2024-04-09 15:49:28 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+// write your mogo compass folder address link where you want to save your data in follow schema structure
+const connect = mongoose.connect("");
+
+// Check database connected or not
+connect.then(() => {
+    console.log("Database Connected Successfully");
+})
+.catch(() => {
+    console.log("Database cannot be Connected");
+})
+
+// Create Schema
+const Loginschema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// collection part
+const collection = new mongoose.model("users", Loginschema);
+
+module.exports = collection;
